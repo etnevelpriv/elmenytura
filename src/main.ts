fc49cc7317c7d129a34c444b646472db2e494cb4
etnevelpriv <leventerastovits@gmail.com>
2026-02-18 11:34:28 +0100

    beolvasas mukodik

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 import "./style.css";
+
+const getFetchTickets = async function () {
+  const ticketPromise = new Promise(async (resolve, reject) => {
+    try {
+      const resp = await fetch("./tickets.json");
+      const data = await resp.json();
+      console.log(data);
+    } catch (err:any) {
+      reject(new Error(err))
+    };
+  });
+  return(ticketPromise);
+
+};
+
+const init = function () {
+  getFetchTickets()
+};
+
+document.addEventListener("DOMContentLoaded", init);
